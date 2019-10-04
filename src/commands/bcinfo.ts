e89ffcd64c654d1d356cf2651fdd6c0ad86be950
TheEmrio <emrio@emrio.fr>
2019-10-04 21:05:59 +0200

    moved some text to config in info and bcinfo

--- src/commands/bcinfo.ts
+++ src/commands/bcinfo.ts
@@ -1,12 +1,9 @@
 import { Message } from 'discord.js'
 import { sendEmbed } from '../methods/embeds'
 import { currentBroadcast } from '../methods/liveRadio'
+import { format } from '../methods/format'
+import utils from '../utils'
 
 export async function bcinfo (message: Message): Promise<void> {
-  sendEmbed(message, [
-    {
-      title: `:musical_note: **Currently playing** (${currentBroadcast.time})`,
-      body: `**${currentBroadcast.author}**\n${currentBroadcast.title}`
-    }
-  ])
+  sendEmbed(message, format(utils.config.messages.bcinfo, currentBroadcast))
 }
--- src/commands/info.ts
+++ src/commands/info.ts
@@ -2,23 +2,14 @@ import { Message } from 'discord.js'
 import { Bot } from '../Bot'
 import { sendEmbed } from '../methods/embeds'
 import { parseTimelapse } from '../methods/parseTimelapse'
+import { format } from '../methods/format'
 import utils from '../utils'
 
 export async function info (message: Message): Promise<void> {
-  sendEmbed(message, [
-    {
-      title: ':information_source: **Radio Classique**',
-      body: `
-        Bot Discord pour écouter Radio Classique
-        [Radio Classique](${utils.config.radioclassique.website})
-        [GitHub](https://github.com/TheEmrio/discord-radioclassique)`
-    },
-    {
-      title: ':tools: **Information technique**',
-      body: `
-        Libraries: ${utils.dependencies.map(dep => `\`${dep}\``)}
-        Uptime: **${parseTimelapse(Bot.uptime)}**
-        Deployed version: **v${utils.packageJson.version}**`
-    }
-  ])
+  sendEmbed(message, format(utils.config.messages.info, {
+    website: utils.config.radioclassique.website,
+    dependencies: utils.dependencies.map(dep => `\`${dep}\``),
+    uptime: parseTimelapse(Bot.uptime),
+    version: utils.packageJson.version
+  }))
 }
