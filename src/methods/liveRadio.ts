359b82cc8148c74445f38868d45f3cf7e3fe6869
TheEmrio <emrio@emrio.fr>
2019-10-04 19:45:07 +0200

    added playRadio @ methods/liveRadio

--- src/methods/liveRadio.ts
+++ src/methods/liveRadio.ts
@@ -1,4 +1,5 @@
 import { PassThrough } from 'stream'
+import { VoiceChannel, StreamDispatcher } from 'discord.js'
 import request from 'request'
 import utils from '../utils'
 
@@ -7,3 +8,9 @@ export const radio = new PassThrough()
 request.get(utils.config.radioclassique.liveradio)
   .on('error', console.error)
   .pipe(radio)
+
+export async function playRadio (channel: VoiceChannel): Promise<StreamDispatcher> {
+  return channel.connection.playStream(radio)
+    .on('end', () => console.log('end'))
+    .on('error', console.error)
+}
