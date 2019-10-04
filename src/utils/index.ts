20bd1a6442c6a9b41b0af5c048176185685aca89
TheEmrio <emrio@emrio.fr>
2019-10-04 19:03:17 +0200

    added package.json to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import debug from './debug'
 import config from './config'
+import packageJson from '../../package.json'
 
-export default { debug, config }
+export default { debug, config, packageJson }
