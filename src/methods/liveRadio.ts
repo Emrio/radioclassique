36c13d23483b627748cd10e7c6660ab92e103615
TheEmrio <emrio@emrio.fr>
2019-10-04 20:06:31 +0200

    Update liveRadio.ts

--- src/methods/liveRadio.ts
+++ src/methods/liveRadio.ts
@@ -4,13 +4,28 @@ import request from 'request'
 import utils from '../utils'
 
 export const radio = new PassThrough()
-
-request.get(utils.config.radioclassique.liveradio)
-  .on('error', console.error)
-  .pipe(radio)
+export const currentBroadcast = { author: 'N/A', title: 'N/A', time: 'N/A' }
 
 export async function playRadio (channel: VoiceChannel): Promise<StreamDispatcher> {
   return channel.connection.playStream(radio)
     .on('end', () => console.log('end'))
     .on('error', console.error)
 }
+
+async function updateCurrentBroadcast () {
+  request.get(utils.config.radioclassique.updates)
+    .on('error', console.error)
+    .on('data', data => {
+      const update = JSON.parse(data.toString())
+      currentBroadcast.author = update.auteur || 'N/A'
+      currentBroadcast.title = update.titre ? update.titre.replace('%\\n', '\n') : 'N/A'
+      currentBroadcast.time = update.duree || 'N/A'
+    })
+}
+
+request.get(utils.config.radioclassique.liveradio)
+  .on('error', console.error)
+  .pipe(radio)
+
+setInterval(() => updateCurrentBroadcast().catch(console.error), utils.config.updateInterval)
+updateCurrentBroadcast()
