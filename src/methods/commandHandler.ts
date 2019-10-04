6a14a65b1e453fcb76fe55e884f4daff3ba9b586
TheEmrio <emrio@emrio.fr>
2019-10-04 18:59:56 +0200

    updated command handler

--- src/methods/commandHandler.ts
+++ src/methods/commandHandler.ts
@@ -1,5 +1,7 @@
 import { Client, Message } from 'discord.js'
+import { sendEmbed } from './embeds'
 import { commands, Command } from '../commands'
+import utils from '../utils'
 
 export function setup (bot: Client): void {
   bot.on('message', commandHandler)
@@ -10,8 +12,12 @@ function commandHandler (message: Message): void {
   const guildPrefix = '$' // TODO: Get with db
   if (!message.content.startsWith(guildPrefix)) return
   const command = message.content.substring(guildPrefix.length).split(' ')[0]
-  console.log(command, Object.keys(commands))
   if (Object.keys(commands).includes(command)) {
     commands[command as Command](message)
+      .catch(err => {
+        console.error(err)
+        return sendEmbed(message, utils.config.messages.error)
+      })
+      .catch(console.error)
   }
 }
