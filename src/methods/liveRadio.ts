6993bb4680dcab07d6cfc56320b56924071bf4e9
TheEmrio <emrio@emrio.fr>
2019-10-09 00:08:24 +0200

    added safety measures + speed measure system

--- src/methods/liveRadio.ts
+++ src/methods/liveRadio.ts
@@ -5,12 +5,19 @@ import utils from '../utils'
 const debug = utils.debug('liveRadio')
 
 export const radio = new PassThrough()
-export const currentBroadcast = { author: 'N/A', title: 'N/A', time: 'N/A' }
+export const currentBroadcast = { author: 'N/A', title: 'N/A', time: 'N/A', speed: -1 }
 
 export async function playRadio (channel: VoiceChannel): Promise<StreamDispatcher> {
   debug('Stream started at %o / %o', channel.guild.id, channel.id)
   return channel.connection.playStream(radio)
-    .on('end', (reason) => debug('Stream ended at %o / %o - Reason: %o', channel.guild.id, channel.id, reason))
+    .on('end', (reason) => {
+      debug('Stream ended at %o / %o - Reason: %o', channel.guild.id, channel.id, reason)
+      if (reason !== 'user') {
+        debug('Reconnecting to stream...')
+        stopBroadcast(channel) // XXX: May be unneccessary
+        playRadio(channel)
+      }
+    })
     .on('error', console.error)
 }
 
@@ -29,9 +36,42 @@ async function updateCurrentBroadcast () {
     })
 }
 
-request.get(utils.config.radioclassique.liveradio)
-  .on('error', console.error)
-  .pipe(radio)
+async function start () {
+  let curDownloadedContentLength = 0
+  let lastLengthMeasures = [ 0 ]
+  setInterval(() => {
+    lastLengthMeasures.push(curDownloadedContentLength)
+    // only keep the latest measures
+    lastLengthMeasures = lastLengthMeasures.slice(-5 / utils.config.updateSpeed)
+    // compute average speed of the latest measures
+    currentBroadcast.speed = lastLengthMeasures.reduce((a, c) => a + c, 0) / (lastLengthMeasures.length * utils.config.updateSpeed)
+    curDownloadedContentLength = 0
+  }, utils.config.updateSpeed * 1000)
 
+  while (true) {
+    debug('Starting service...')
+    // this promise won't resolve until the connection to the radio crashes
+    await new Promise((resolve) => {
+      try {
+        request.get(utils.config.radioclassique.liveradio)
+          .on('data', data => {
+            curDownloadedContentLength += data.length
+          })
+          .on('error', (err) => {
+            console.error(err)
+            debug('Fatal error: restarting service...')
+            return setTimeout(resolve, 1000)
+          })
+          .pipe(radio)
+      } catch (err) {
+        console.error(err)
+        debug('Fatal error: restarting service...')
+        setTimeout(resolve, 1000)
+      }
+    })
+  }
+}
+
+start()
 setInterval(() => updateCurrentBroadcast().catch(console.error), utils.config.updateInterval)
 updateCurrentBroadcast()
